c0787810f963efc0d74bfb35e47584fb4364b691
mohamedmazeen <mohamed.mazeen35@gmail.com>
2023-07-29 13:00:53 +0800

    second-commit

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@
 * @date - 28/7/2023 
 **/
 function counter() {
-    var characters = document.getElementById("input").value;
+    var characters = document.getElementById("text-field").value;
     characters = characters.length;
     document.getElementById("result").innerHTML = characters;
 }
